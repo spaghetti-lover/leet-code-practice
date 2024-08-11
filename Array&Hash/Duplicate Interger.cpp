912ca2d49047dafdd947fe929955c6a097b1d272
spaghetti-lover <phungducanh2511@gmail.com>
2024-08-11 11:03:08 +0700

    init

---
+++ Array&Hash/Duplicate Interger.cpp
@@ -0,0 +1,11 @@
+// https://neetcode.io/problems/duplicate-integer
+
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+        hashset = set()
+
+        for n in nums:
+            if n in hashset:
+                return True
+            hashset.add(n)
+        return False
