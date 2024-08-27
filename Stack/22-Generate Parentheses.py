ff68bb61a8f9ee9a5aa50cd60dba34d066bb22cd
spaghetti-lover <phungducanh2511@gmail.com>
2024-08-27 21:58:34 +0700

    Day 9 of leetcode

---
+++ Stack/22-Generate Parentheses.py
@@ -0,0 +1,19 @@
+# Time complexity: O(2^n)
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        stack = []
+        res = []
+
+        def backtrack(openN, closedN):
+            if openN == closedN == n:
+                res.append("".join(stack))
+            if openN < n:
+                stack.append("(")
+                backtrack(openN + 1, closedN)
+                stack.pop()
+            if openN > closedN:
+                stack.append(")")
+                backtrack(openN, closedN + 1)
+                stack.pop()
+        backtrack(0, 0)
+        return res
