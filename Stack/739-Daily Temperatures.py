3aeabd5f3f94714184caabfc9f000f9577120eca
spaghetti-lover <phungducanh2511@gmail.com>
2024-08-27 22:30:58 +0700

    Day 9 of leetcode

---
+++ Stack/739-Daily Temperatures.py
@@ -0,0 +1,12 @@
+# Time complexity: O(n)
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        stack = []
+        res = [0] * len(temperatures)
+        for i in range(len(temperatures)):
+            while len(stack) > 0 and temperatures[i] > temperatures[stack[-1]]:
+                res[stack[-1]] = i - stack[-1]
+                stack.pop()
+            stack.append(i)
+        return res
+        
